671add83add76e1fff1ffd00245526a26f4073ba
李乾坤 <542154968@qq.com>
2019-07-02 11:32:22 +0800

    feat: setAttritube 和 updateAttritube 兼容

--- src/platforms/web/runtime/modules/attrs.js
+++ src/platforms/web/runtime/modules/attrs.js
@@ -14,12 +14,14 @@ import {
   getXlinkProp,
   isBooleanAttr,
   isEnumeratedAttr,
+  // val ===null || val === false
   isFalsyAttrValue,
   convertEnumeratedValue
 } from 'web/util/index'
 
 function updateAttrs (oldVnode: VNodeWithData, vnode: VNodeWithData) {
   const opts = vnode.componentOptions
+  // 没有内联attrs或者没有attrs return
   if (isDef(opts) && opts.Ctor.options.inheritAttrs === false) {
     return
   }
@@ -43,15 +45,21 @@ function updateAttrs (oldVnode: VNodeWithData, vnode: VNodeWithData) {
     }
   }
   // #4391: in IE9, setting type can reset value for input[type=radio]
+  // IE9 设置input[type=radio]的type会重设value
   // #6666: IE/Edge forces progress value down to 1 before setting a max
+  // IE/Edge 迫使 progress 的value 下降到1 在设置max之前  在设置最大值之前将进度值强制降至1
   /* istanbul ignore if */
   if ((isIE || isEdge) && attrs.value !== oldAttrs.value) {
     setAttr(elm, 'value', attrs.value)
   }
   for (key in oldAttrs) {
+    // 如果value不存在
     if (isUndef(attrs[key])) {
+      // 如果是xlink
       if (isXlink(key)) {
+        // 移除 namespace的xlnk
         elm.removeAttributeNS(xlinkNS, getXlinkProp(key))
+        // 如果key存在  移除key
       } else if (!isEnumeratedAttr(key)) {
         elm.removeAttribute(key)
       }
@@ -63,12 +71,15 @@ function setAttr (el: Element, key: string, value: any) {
   // 如果含有-
   if (el.tagName.indexOf('-') > -1) {
     baseSetAttr(el, key, value)
+    // 如果值是布尔类型的key
   } else if (isBooleanAttr(key)) {
     // set attribute for blank value
     // e.g. <option disabled>Select one</option>
+    // 如果是false 或null 移除key
     if (isFalsyAttrValue(value)) {
       el.removeAttribute(key)
     } else {
+      // 兼容 iframe 和 flash
       // technically allowfullscreen is a boolean attribute for <iframe>,
       // but Flash expects a value of "true" when used on <embed> tag
       value = key === 'allowfullscreen' && el.tagName === 'EMBED'
@@ -76,14 +87,18 @@ function setAttr (el: Element, key: string, value: any) {
         : key
       el.setAttribute(key, value)
     }
+    // contenteditable,draggable,spellcheck
+    // 这些需要设置成字符串
   } else if (isEnumeratedAttr(key)) {
     el.setAttribute(key, convertEnumeratedValue(key, value))
+    // 如果是xlink
   } else if (isXlink(key)) {
     if (isFalsyAttrValue(value)) {
       el.removeAttributeNS(xlinkNS, getXlinkProp(key))
     } else {
       el.setAttributeNS(xlinkNS, key, value)
     }
+    // 其余的就是通用的了
   } else {
     baseSetAttr(el, key, value)
   }
