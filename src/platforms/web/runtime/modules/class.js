a2c3b79f9f86bc8c8747cc10165cf296ce3ad433
李乾坤 <542154968@qq.com>
2019-07-02 11:41:27 +0800

    feat: class 的设置

--- src/platforms/web/runtime/modules/class.js
+++ src/platforms/web/runtime/modules/class.js
@@ -15,6 +15,7 @@ function updateClass (oldVnode: any, vnode: any) {
   const el = vnode.elm
   const data: VNodeData = vnode.data
   const oldData: VNodeData = oldVnode.data
+  // 这一大坨 差评 
   if (
     isUndef(data.staticClass) &&
     isUndef(data.class) && (
@@ -27,15 +28,18 @@ function updateClass (oldVnode: any, vnode: any) {
     return
   }
 
+  // 获取所有的class
   let cls = genClassForVnode(vnode)
 
   // handle transition classes
   const transitionClass = el._transitionClasses
+  // 获取transition的class
   if (isDef(transitionClass)) {
     cls = concat(cls, stringifyClass(transitionClass))
   }
 
   // set the class
+  // 设置class
   if (cls !== el._prevClass) {
     el.setAttribute('class', cls)
     el._prevClass = cls
