dce560d90d5dee579b937e17fee4c5d1d6231b93
李乾坤 <542154968@qq.com>
2019-07-02 09:52:18 +0800

    feat: transition的实现
    key的关系 v-show和transition的关系

--- src/platforms/web/runtime/components/transition.js
+++ src/platforms/web/runtime/components/transition.js
@@ -43,30 +43,37 @@ function getRealChild (vnode: ?VNode): ?VNode {
   }
 }
 
+// 提取transition的data
 export function extractTransitionData (comp: Component): Object {
   const data = {}
   const options: ComponentOptions = comp.$options
+  // 复制props
   // props
   for (const key in options.propsData) {
     data[key] = comp[key]
   }
   // events.
   // extract listeners and pass them directly to the transition methods
+  // 提取侦听器并将其直接传递给转换方法
   const listeners: ?Object = options._parentListeners
   for (const key in listeners) {
+    // 格式化key
     data[camelize(key)] = listeners[key]
   }
   return data
 }
 
+// 占位 返回一个含有props的keepalive
 function placeholder (h: Function, rawChild: VNode): ?VNode {
   if (/\d-keep-alive$/.test(rawChild.tag)) {
+    // render函数
     return h('keep-alive', {
       props: rawChild.componentOptions.propsData
     })
   }
 }
 
+// 判断父级是否含有transition  只要父级以上有一个就返回true
 function hasParentTransition (vnode: VNode): ?boolean {
   while ((vnode = vnode.parent)) {
     if (vnode.data.transition) {
@@ -75,19 +82,22 @@ function hasParentTransition (vnode: VNode): ?boolean {
   }
 }
 
+// 判断子是否一样
 function isSameChild (child: VNode, oldChild: VNode): boolean {
   return oldChild.key === child.key && oldChild.tag === child.tag
 }
 
+//  node.isComment && node.asyncFactory 判断是不是字符串node
 const isNotTextNode = (c: VNode) => c.tag || isAsyncPlaceholder(c)
-
+// 干嘛用的 v-show自定义指令？
 const isVShowDirective = d => d.name === 'show'
 
 export default {
   name: 'transition',
   props: transitionProps,
+  // 抽象组件
   abstract: true,
-
+  // render函数
   render (h: Function) {
     let children: any = this.$slots.default
     if (!children) {
@@ -95,13 +105,16 @@ export default {
     }
 
     // filter out text nodes (possible whitespaces)
+    // 过滤文本节点
     children = children.filter(isNotTextNode)
     /* istanbul ignore if */
+    // 过滤之后如果没有就return
     if (!children.length) {
       return
     }
 
     // warn multiple elements
+    // 这就是报错transition有多个节点时候的信息
     if (process.env.NODE_ENV !== 'production' && children.length > 1) {
       warn(
         '<transition> can only be used on a single element. Use ' +
@@ -113,6 +126,7 @@ export default {
     const mode: string = this.mode
 
     // warn invalid mode
+    // mode类型报错
     if (process.env.NODE_ENV !== 'production' &&
       mode && mode !== 'in-out' && mode !== 'out-in'
     ) {
@@ -126,18 +140,22 @@ export default {
 
     // if this is a component root node and the component's
     // parent container node also has transition, skip.
+    // 如果父节点已经有transition了 就跳过直接返回children
     if (hasParentTransition(this.$vnode)) {
       return rawChild
     }
 
     // apply transition data to child
     // use getRealChild() to ignore abstract components e.g. keep-alive
+    // 获取真实节点 非抽象的
     const child: ?VNode = getRealChild(rawChild)
     /* istanbul ignore if */
+    // 如果没有 就返回
     if (!child) {
       return rawChild
     }
 
+    // _leaving?离开 ？ 如果这个状态 返回占位keepalive
     if (this._leaving) {
       return placeholder(h, rawChild)
     }
@@ -146,11 +164,16 @@ export default {
     // component instance. This key will be used to remove pending leaving nodes
     // during entering.
     const id: string = `__transition-${this._uid}-`
+    // 如果是组件 如果child.key是null 返回id 不是组件返回tag
+    // child.key不是null 返回
+    // key的设置
     child.key = child.key == null
       ? child.isComment
         ? id + 'comment'
         : id + child.tag
+        // key是基本类型
       : isPrimitive(child.key)
+      // 
         ? (String(child.key).indexOf(id) === 0 ? child.key : id + child.key)
         : child.key
 
@@ -160,10 +183,15 @@ export default {
 
     // mark v-show
     // so that the transition module can hand over the control to the directive
+    // 所以，transition模块可以在手的控制指令到 
+
+    // 如果子的directives存在  并且指令中含有v-show
+    /**transition和v-show配合 */
     if (child.data.directives && child.data.directives.some(isVShowDirective)) {
+      // 那么就展示出来
       child.data.show = true
     }
-
+    // 如果老的子存在 而且含有data 而且和新的子不一样  node.isComment && node.asyncFactory ？ 并且老的子不是组件
     if (
       oldChild &&
       oldChild.data &&
@@ -173,23 +201,28 @@ export default {
       !(oldChild.componentInstance && oldChild.componentInstance._vnode.isComment)
     ) {
       // replace old child transition data with fresh one
+      // 用新的子转换数据替换旧的子转换数据
       // important for dynamic transitions!
+      // 对于动态转换很重要！
       const oldData: Object = oldChild.data.transition = extend({}, data)
       // handle transition mode
       if (mode === 'out-in') {
         // return placeholder node and queue update when leave finishes
         this._leaving = true
+        // 将oldData和afterLeave hook合在一起
         mergeVNodeHook(oldData, 'afterLeave', () => {
           this._leaving = false
           this.$forceUpdate()
         })
         return placeholder(h, rawChild)
+        // 如果是in-out
       } else if (mode === 'in-out') {
         if (isAsyncPlaceholder(child)) {
           return oldRawChild
         }
         let delayedLeave
         const performLeave = () => { delayedLeave() }
+        // 数据和hook 绑定
         mergeVNodeHook(data, 'afterEnter', performLeave)
         mergeVNodeHook(data, 'enterCancelled', performLeave)
         mergeVNodeHook(oldData, 'delayLeave', leave => { delayedLeave = leave })
