b96695f0ecc7d0ad035c1538639025921aece514
李乾坤 <542154968@qq.com>
2019-07-02 10:50:55 +0800

    feat: addClass 和 removeClass的封装

--- src/platforms/web/runtime/class-util.js
+++ src/platforms/web/runtime/class-util.js
@@ -6,6 +6,7 @@ const whitespaceRE = /\s+/
  * Add class with compatibility for SVG since classList is not supported on
  * SVG elements in IE
  */
+// addClass的封装
 export function addClass (el: HTMLElement, cls: ?string) {
   /* istanbul ignore if */
   if (!cls || !(cls = cls.trim())) {
@@ -31,6 +32,7 @@ export function addClass (el: HTMLElement, cls: ?string) {
  * Remove class with compatibility for SVG since classList is not supported on
  * SVG elements in IE
  */
+// removeClass的封装
 export function removeClass (el: HTMLElement, cls: ?string) {
   /* istanbul ignore if */
   if (!cls || !(cls = cls.trim())) {
