5bb6f322d43789ddb1ac582cb69b1300fa2dd2d1
李乾坤 <542154968@qq.com>
2019-07-08 13:20:16 +0800

    feat: v-on转字符串function

--- src/compiler/directives/on.js
+++ src/compiler/directives/on.js
@@ -2,6 +2,7 @@
 
 import { warn } from 'core/util/index'
 
+// v-on转function的实现
 export default function on (el: ASTElement, dir: ASTDirective) {
   if (process.env.NODE_ENV !== 'production' && dir.modifiers) {
     warn(`v-on without argument does not support modifiers.`)
